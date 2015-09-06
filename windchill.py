23af1bce09d1142de182c2fc5d7c86774694587d
rowanv <rrvspam@gmail.com>
2015-09-06 16:49:09 -0500

    added mappings and started bokeh viz creation for windchill

--- windchill.py
+++ windchill.py
@@ -27,3 +27,48 @@ windchill_array = np.array(windchill_values )
 windchill_array = np.reshape(windchill_array, (20, 13))
 
 windchill_df = pd.DataFrame(data=windchill_array, index=amb_temp, columns=windspeed)
+
+def windchill_to_discrete(twc):
+	if twc > 0 :
+		return 'Greater than 0'
+	elif twc > -10:
+		return '-10 to 0'
+	elif twc > -20:
+		return '-20 to -10'
+	elif twc > -30:
+		return '-30 to -20'
+	elif twc > -40:
+		return '-40 to -30'
+	elif twc > -50:
+		return '-50 to -40'
+	elif twc > -60:
+		return '-60 to -50'
+	else:
+		return 'NaN'
+
+windchill_discrete = map(lambda x: windchill_to_discrete(twc), windchill_df.values
+
+windspeed_range = [str(c) for c in windchill_df.columns]
+amb_temp_range = [str(i) for i in windchill_df.index]
+
+
+colormap = {
+	'Greater than 0': '#ffffcc',
+	'-10 to 0': '#a1dab4',
+	'-20 to -10': '#41b6c4',
+	'-30 to -20': '#225ea8',
+	'-40 to -30': '#bdbdbd',
+	'-50 to -40': '#bdbdbd',
+	'-60 to -50': '#bdbdbd',
+	'NaN': '#bdbdbd',
+}
+
+output_file('windchill_table.html')
+
+p = figure(title='Windchill', tools='resize,hover,save', x_range=windspeed_range, y_range=amb_temp_range)
+p.plot_width = 1200
+p.toolbar_location = 'left'
+
+
+
+
